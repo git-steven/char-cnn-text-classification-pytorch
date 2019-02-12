48340356ebea5b81c675133ef7f036c14915f69a
Steven Miers <smiers@glgroup.com>
2019-02-12 13:45:54 -0600

    upgrade to work with torch 1.0.1

--- model.py
+++ model.py
@@ -15,8 +15,8 @@ class  CharCNN(nn.Module):
             nn.Conv1d(256, 256, kernel_size=7, stride=1),
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=3, stride=3)
-        )            
-            
+        )
+
         self.conv3 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
             nn.ReLU()
@@ -24,9 +24,9 @@ class  CharCNN(nn.Module):
 
         self.conv4 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
-            nn.ReLU()    
+            nn.ReLU()
         )
-        
+
         self.conv5 = nn.Sequential(
             nn.Conv1d(256, 256, kernel_size=3, stride=1),
             nn.ReLU()
@@ -37,14 +37,14 @@ class  CharCNN(nn.Module):
             nn.ReLU(),
             nn.MaxPool1d(kernel_size=3, stride=3)
         )
-            
-        
+
+
         self.fc1 = nn.Sequential(
             nn.Linear(8704, 1024),
             nn.ReLU(),
             nn.Dropout(p=args.dropout)
         )
-        
+
         self.fc2 = nn.Sequential(
             nn.Linear(1024, 1024),
             nn.ReLU(),
@@ -52,7 +52,7 @@ class  CharCNN(nn.Module):
         )
 
         self.fc3 = nn.Linear(1024, 4)
-        self.log_softmax = nn.LogSoftmax()
+        self.log_softmax = nn.LogSoftmax(0)
 
     def forward(self, x):
         x = self.conv1(x)
@@ -72,5 +72,5 @@ class  CharCNN(nn.Module):
         x = self.fc3(x)
         # output layer
         x = self.log_softmax(x)
-        
+
         return x
